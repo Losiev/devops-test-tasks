7d64d13662b5daccf33d04d0847f761e92648aa1
Vladyslav Gusakovskiy <vladyslav.gusakovskiy@gmail.com>
2025-11-05 21:10:56 +0200

    added HEALTHCHECK and reduced image size to alpine

--- tasks/docker-task-1/app.py
+++ tasks/docker-task-1/app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello():
     return "Hello, DevOps with Gunicorn!"
 
+@app.route("/health")
+def health():
+    return "OK", 200   
+
 if __name__ == "__main__":
     app.run()
